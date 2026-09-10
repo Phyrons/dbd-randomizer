9a3f031575cc19cf7b68b7d2561729da5d29b52a
Phyrons <101155442+Phyrons@users.noreply.github.com>
2026-09-10 10:32:43 -0500

    Refactor: añadir configuración de módulos JS

---
+++ scripts/config.js
@@ -0,0 +1,36 @@
+export const ITEM_WIDTH = 280;
+export const MAX_RULETA = 50;
+export const TOTAL_SLOTS = 4;
+
+export const SIN_PERK = {
+    sinPerk: true,
+    nombre: "Sin Perk",
+    nombre_es: "Sin Perk",
+    personaje: "",
+    archivo: null
+};
+
+export const mensajesChat = {
+    inicioGiro: {
+        eng: "¡Starting roll for category: ",
+        esp: "¡Comenzando giro para categoría: "
+    },
+    rerollDenegadoPorRuleta: {
+        eng: "Reroll blocked: Wheel is spinning.",
+        esp: "Reroll denegado: Ruleta en curso."
+    },
+    rerollDenegado: {
+        eng: "Reroll denied: You must finish the initial roll first.",
+        esp: "Reroll denegado: Debes terminar el giro inicial primero."
+    },
+    rerollIniciado: {
+        eng: "Reroll started for slot: ",
+        esp: "Reroll iniciado para el slot: "
+    },
+    idioma: {
+        eng: "Spinner Language set to English",
+        esp: "Idioma del Spinner actualizado a Espanol"
+    }
+};
+
+export const CATEGORIAS = ["perksurv", "perkkiller"];
